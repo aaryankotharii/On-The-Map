0bc168b9707f561713793d421c08c90a6a655c42
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-04 12:37:10 +0530

    newStudentLocation response struct added

---
+++ On The Map/Model/Response/NewStudentLocationResponse.swift
@@ -0,0 +1,15 @@
+//
+//  NewStudentLocationResponse.swift
+//  On The Map
+//
+//  Created by Aaryan Kothari on 04/05/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import Foundation
+
+
+struct newStudentLocation : Codable{
+     var objectId : String
+     var createdAt : String
+}
