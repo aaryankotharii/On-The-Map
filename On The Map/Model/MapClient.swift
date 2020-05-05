56f6e09e6c15c759be139000a697945746378538
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-05 21:59:42 +0530

    MapVC updated

--- On The Map/Model/MapClient.swift
+++ On The Map/Model/MapClient.swift
@@ -61,35 +61,5 @@ class MapClient : NSObject, MKMapViewDelegate{
                mapView.addAnnotation(annotation)
         }
     }
-    
 
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
-        let reuseId = "pin"
-        
-        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
-
-        if pinView == nil {
-            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
-            pinView!.canShowCallout = true
-            pinView!.pinColor = .red
-            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-        }
-        else {
-            pinView!.annotation = annotation
-        }
-        
-        return pinView
-    }
-    
-    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-        if control == view.rightCalloutAccessoryView {
-            let app = UIApplication.shared
-            if let toOpen = view.annotation?.subtitle! {
-                app.openURL(URL(string: toOpen)!)
-            }
-        }
-    }
-    
-    
 }
