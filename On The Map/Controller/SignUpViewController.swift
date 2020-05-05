58ffb7dfb560c81eeee819ec748755c1f8799806
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-05 19:41:46 +0530

    SignUpVC complete

--- On The Map/Controller/SignUpViewController.swift
+++ On The Map/Controller/SignUpViewController.swift
@@ -7,24 +7,30 @@
 //
 
 import UIKit
+import WebKit
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let url = URL(string: "https://auth.udacity.com/sign-up")
+        showWebsite(url!)
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func doneClicked(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    func showWebsite(_ url : URL){
+                        
+        webView.navigationDelegate = self
+                        
+        webView.load(URLRequest(url: url))
+        
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    
 }
