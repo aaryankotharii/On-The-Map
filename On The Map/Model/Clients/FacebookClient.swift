775a3874d36ad774d66b44a1ff7734b013c985b0
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-07 02:33:32 +0530

    MAPClient + FBClient code refactored

--- On The Map/Model/Clients/FacebookClient.swift
+++ On The Map/Model/Clients/FacebookClient.swift
@@ -10,9 +10,10 @@ import UIKit
 import FacebookLogin
 import FBSDKLoginKit
 
-
+//MARK:- ALL FACEBOOK RELATED FUNCTIONS
 class FacebookClient {
-        
+    
+    //FACEBOOK LOGIN
     class func fbLogin(vc : UIViewController,completion: @escaping (Bool, Error?) -> Void){
         let loginManager = LoginManager()
         loginManager.logOut()
@@ -31,33 +32,38 @@ class FacebookClient {
         }
     }
     
+    
+    //GET DATA ( NAME + EMAIL + ETC )
     class func getUserData(completion: @escaping (Bool, fbData ,Error?) -> Void){
         if((AccessToken.current) != nil){
-                  GraphRequest(graphPath: "me", parameters: ["fields": "id, name, picture.type(large), email, gender"]).start(completionHandler: { (connection, result, error) -> Void in
-                    
-                    if let error = error {
-                        print(error.localizedDescription)
-                        completion(false,fbData(),error)
-                        return
-                    }
-                          
-                          let dict = result as! [String : AnyObject]
-                          let picutreDic = dict as NSDictionary
-                          
-                          let name = picutreDic.object(forKey: "name") as! String
-                          let emailAddress = picutreDic.object(forKey: "email") as! String
-                   
-                    completion(true,fbData(name: name, email: emailAddress), nil)
-                  })
-              }
-          }
+            GraphRequest(graphPath: "me", parameters: ["fields": "id, name, picture.type(large), email, gender"]).start(completionHandler: { (connection, result, error) -> Void in
+                
+                if let error = error {
+                    print(error.localizedDescription)
+                    completion(false,fbData(),error)
+                    return
+                }
+                
+                let dict = result as! [String : AnyObject]
+                let picutreDic = dict as NSDictionary
+                
+                let name = picutreDic.object(forKey: "name") as! String
+                let emailAddress = picutreDic.object(forKey: "email") as! String
+                
+                completion(true,fbData(name: name, email: emailAddress), nil)
+            })
+        }
+    }
     
+    //LOGOUT
     class func logout(){
         let loginManager = LoginManager()
         loginManager.logOut()
     }
 }
 
+
+//MARK:- Struct to get data
 struct fbData{
     var name : String = ""
     var email : String = ""
